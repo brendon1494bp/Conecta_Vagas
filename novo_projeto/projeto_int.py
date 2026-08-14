820bef8f3f8c246d3e21436b4f9f8e5cf2819776
MILLENA Almeida <millena64338576@edu.df.senac.br>
2026-08-14 11:07:45 -0300

    PARTE millena

--- novo_projeto/projeto_int.py
+++ novo_projeto/projeto_int.py
@@ -1,18 +1,49 @@
-from flask import flask, render_template,request,redirect,url_for,session,jsonify
-import mysql.connector
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask,request,jsonify
+from flask_mysqldb import MySQL
+from flask_cors import CORS
 
-app = flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
-app.secret_key = 'sua_chave_secreta_aqui'
 
-def get_db():
-    return mysql.connector.connect(
-        host='localhost',
-        port=3307,
-        user='root',
-        password='senac',
-        database='sistema_vagas_gdf'
+app.config['MYSQl_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'senha'
+app.config['MYSQL_DB'] = 'sistema'
+
+mysql = MySQL(app)
+
+@app.route('/usuarios' , methods=['POST'])
+def criar_usuario():
+    nome = request.json['nome']
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "INSERT INTO usuarios(nome) VALUES(%s)" ,
+        (nome,)
     )
+    mysql.connection.commit()
+    return jsonify({"mensagem": "Usuario criado"})
 
-@pp.route('/')
+app.route('/usuarios' , methods=['GET'])
+def listar_usuarios():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM usuarios")
+    dados = cur.fetchall()
+
+    usuarios = [
+        {"id": u[0], "nome": u[1]}
+        for u in dados
+
+    ]
+
+    return jsonify(usuarios)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
