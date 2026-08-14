429bf8cffa8036e5286c5575d55d290dfc74eb9f
brendon1494bp <brendon1494.bp@gmail.com>
2026-08-14 09:40:16 -0300

    vamos começar

--- novo_projeto/projeto_int.py
+++ novo_projeto/projeto_int.py
@@ -1,18 +1,32 @@
-from flask import flask, render_template,request,redirect,url_for,session,jsonify
-import mysql.connector
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+app = Flask(__name__)
+app.secret_key= "chave_simples"
 
-app = flask(__name__)
+@app.route('/')
+def home():
+    usuario_logado = session.get('usuario_logado')
+    return render_template('index.html',nome=usuario_logado)
 
-app.secret_key = 'sua_chave_secreta_aqui'
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+    if email == 'admin@gmail.com' and senha == '123456':
+        session ["usuario"] = "brendon"
+        return redirect(url_for('home'))
+    return "email ou senha incorretos" , 400
+@app.route('/api/escolas')
+def get_escolas():
+    escolas = [
+        {"id": 1, "nome": "Escola Classe 01"},
+        {"id": 2, "nome": "Centro de Ensino Médio 02"}
+    ]
+    return jsonify(escolas)
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
-def get_db():
-    return mysql.connector.connect(
-        host='localhost',
-        port=3307,
-        user='root',
-        password='senac',
-        database='sistema_vagas_gdf'
-    )
-
-@pp.route('/')
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
